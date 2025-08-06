2e4a0cc9ee20bfbf5c023ee0ee5aeee981a3dfd4
Ankit Joshi <theankitjoshi12345@gmail.com>
2025-08-05 21:55:32 -0500

    Initial commit

---
+++ Student.py
@@ -0,0 +1,30 @@
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return (f"{self.name} is {self._age} years old.")
+
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, age):
+        self._age = age
+
+    @classmethod
+    def get(cls):
+        name = input("Enter student's name: ")
+        age = input("Enter Student's age: ")
+        return cls(name, age)
+
+
+def main():
+    student = Student.get()
+    print(student)
+
+
+if (__name__ == "__main__"):
+    main()
